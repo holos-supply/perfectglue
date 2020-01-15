b8e45da50e41d0f79c33af3f8fae94ce87ff6a0f
Rok Pušnik <rok.pusnik@conology.onmicrosoft.com>
2020-01-15 14:32:54 +0100

    fix #39

--- src/main/java/org/perfectglue/controller/JiraCloudController.java
+++ src/main/java/org/perfectglue/controller/JiraCloudController.java
@@ -2,16 +2,19 @@ package org.perfectglue.controller;
 
 import org.perfectglue.config.ConnectionProperties;
 import org.perfectglue.connector.XrayCloudConnector;
-import org.perfectglue.util.MessageHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * The <code>JiraCloudController</code> is a core class used by Spring Boot to parse mappings
+ * 
+ * @author Willm T&uuml;ting
+ *
+ */
 @RestController
 @RequestMapping("/api/cloud")
 public class JiraCloudController {
@@ -26,24 +29,24 @@ public class JiraCloudController {
 		
 		// the magic happens here
 			XrayCloudConnector con = new XrayCloudConnector(properties.getXray().getCloud().getUser(),
-					properties.getXray().getCloud().getPass(),
-					properties.getXray().getCloud().getUrl());
-				
-				// read Xray
-				con.getGherkinFromTestCloud(id);
-				
-				// execute test with cucumber
-				try {
-					cucumber.api.cli.Main.run(defaultOptions, Thread.currentThread().getContextClassLoader());
-				} catch (Throwable e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				
-				logger.info("Publishing results");
-				// public results to Xray
-				con.publishResults(id);
+				properties.getXray().getCloud().getPass(),
+				properties.getXray().getCloud().getUrl());
+			
+			// read Xray
+			con.getGherkinFromTestCloud(id);
+			
+			// execute test with cucumber
+			try {
+				cucumber.api.cli.Main.run(defaultOptions, Thread.currentThread().getContextClassLoader());
+			} catch (Throwable e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			logger.info("Publishing results");
+			// public results to Xray
+			con.publishResults(id);
 	}
 	
 	
