aacdcb9fd7c7abcee3472cebb46fee8cd6b60211
Rok Pušnik <rok.pusnik@conology.onmicrosoft.com>
2020-01-16 10:28:23 +0100

    fix #1

--- src/main/java/org/perfectglue/PerfectGlueApplication.java
+++ src/main/java/org/perfectglue/PerfectGlueApplication.java
@@ -5,7 +5,11 @@ import org.perfectglue.config.QueueProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * The <code>PerfectGlueApplication</code> is the starter class for the entire framework
+ * Starting it will initialise the program and add listeners to the port 8080
+ * 
+ */
 @SpringBootApplication
 public class PerfectGlueApplication {
 	
