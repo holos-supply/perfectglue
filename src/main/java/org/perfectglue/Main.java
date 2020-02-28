ea61725b6dc7eb6c3e40120c8f0ffcfe01d3aaf2
Rok Pušnik <rok.pusnik@conology.net>
2020-02-28 11:57:54 +0100

    tiny formatting change

--- src/main/java/org/perfectglue/Main.java
+++ src/main/java/org/perfectglue/Main.java
@@ -10,7 +10,7 @@ public class Main {
 	public static void main(String[] args) {
 		List<CamundaTask> list = DataHandler.initCamundaTaskList("");
 		for(CamundaTask task : list) {
-			System.out.println("Task: \n" + task.toString());
+			System.out.println("\n\nTask: \n" + task.toString());
 		}
 	}
 }
